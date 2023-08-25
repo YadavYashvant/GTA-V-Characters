a4584fbc3111dc6c616cc3402e4355f54336271e
YadavYashvant <yashvantyadav855@gmail.com>
2023-08-25 18:45:43 +0530

    adding a status bar modification

--- app/src/main/java/com/example/gist_jetpack/ui/theme/Theme.kt
+++ app/src/main/java/com/example/gist_jetpack/ui/theme/Theme.kt
@@ -10,10 +10,26 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+/*
+val systemUiController = rememberSystemUiController()
+if(darkTheme){
+    systemUiController.setSystemBarsColor(
+        color = Color.Transparent
+    )
+}else{
+    systemUiController.setSystemBarsColor(
+        color = Color.White
+    )
+}
+
+ */
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
